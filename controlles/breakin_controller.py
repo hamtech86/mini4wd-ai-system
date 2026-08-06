5ecef8aeb4e2d2de548c13ee7eeae43bccc7da14
hamtech86 <ham_tech_86@hamui.net>
2026-08-07 02:47:44 +0900

    Fix Break-in Controller serial interface compatibility

--- controlles/breakin_controller.py
+++ controlles/breakin_controller.py
@@ -6,6 +6,8 @@ Recipe
  -> Phase Control
  -> Arduino Control
  -> Measurement Collection
+ -> Analysis Engine
+ -> Database
 """
 
 import time
@@ -14,8 +16,19 @@ from .phase_manager import PhaseManager
 
 
 class BreakinController:
+    """
+    Break-in execution controller.
 
-    def __init__(self, serial_controller, measurement_manager=None, analysis_engine=None, database=None):
+    Hardware control remains separated from analysis.
+    """
+
+    def __init__(
+        self,
+        serial_controller,
+        measurement_manager=None,
+        analysis_engine=None,
+        database=None
+    ):
         self.serial = serial_controller
         self.measurement_manager = measurement_manager
         self.analysis_engine = analysis_engine
@@ -25,33 +38,50 @@ class BreakinController:
     def start(self, recipe):
         self.phase_manager = PhaseManager(recipe)
         self.running = True
+
         measurements = []
 
         while self.running and self.phase_manager.has_next():
             phase = self.phase_manager.current_phase()
+
             self.execute_phase(phase)
 
             if self.measurement_manager:
-                measurements.append(self.measurement_manager.collect())
+                measurements.append(
+                    self.measurement_manager.collect()
+                )
 
             self.phase_manager.next_phase()
 
         self.stop()
+
         return measurements
 
     def execute_phase(self, phase):
-        self.serial.set_direction(phase.direction)
+        """Execute one break-in phase."""
+
+        if phase.direction == "REV":
+            self.serial.reverse()
+        else:
+            self.serial.forward()
+
         self.serial.set_pwm(phase.pwm)
 
         start = time.time()
+
         while self.running and time.time() - start < phase.duration_sec:
             time.sleep(0.1)
 
     def stop(self):
         self.running = False
+        self.serial.stop_breakin()
         self.serial.set_pwm(0)
 
     def emergency_stop(self):
-        self.stop()
+        self.running = False
+
+        if hasattr(self.serial, "stop_breakin"):
+            self.serial.stop_breakin()
+
         if hasattr(self.serial, "emergency_stop"):
             self.serial.emergency_stop()
