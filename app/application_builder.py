c773280c96198f9c347976740752a222a182eeca
hamtech86 <ham_tech_86@hamui.net>
2026-08-28 00:56:07 +0900

    fix: wire DatabaseManager into break-in analysis

--- app/application_builder.py
+++ app/application_builder.py
@@ -9,13 +9,13 @@ SerialController
 MeasurementManager
 AnalysisEngine
 BreakinController
-
-The builder keeps dependency wiring outside individual modules.
+DatabaseManager
 """
 
 from analysis.analysis_engine import AnalysisEngine
 from controllers.breakin_controller import BreakinController
 from measurement.measurement_manager import MeasurementManager
+from database.manager.database_manager import DatabaseManager
 
 
 class ApplicationBuilder:
@@ -26,7 +26,8 @@ class ApplicationBuilder:
         self.serial_controller = serial_controller
         self.analysis_engine = analysis_engine
         self.session_manager = session_manager
-        self.database = database
+        self.database = database or DatabaseManager("database/mini4wd.db")
+        self.database.connect()
 
     def build_breakin_controller(self):
         measurement_manager = MeasurementManager(
