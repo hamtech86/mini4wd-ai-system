5c2fe244e050e7a11da9ae1836486ba115d34c3d
hamtech86 <ham_tech_86@hamui.net>
2026-09-05 14:57:17 +0900

    feat: wire Local Raw Log persistence into application builder

--- app/application_builder.py
+++ app/application_builder.py
@@ -14,19 +14,21 @@ The builder keeps dependency wiring outside individual modules.
 """
 
 from analysis.analysis_engine import AnalysisEngine
-from controllers.breakin_controller import BreakinController
+from controllers.local_raw_log_breakin_controller import LocalRawLogBreakinController
 from measurement.measurement_manager import MeasurementManager
+from raw_log_library import RawLogLibrary
 
 
 class ApplicationBuilder:
     """Create application service instances."""
 
     def __init__(self, serial_controller, analysis_engine=None,
-                 session_manager=None, database=None):
+                 session_manager=None, database=None, raw_log_library=None):
         self.serial_controller = serial_controller
         self.analysis_engine = analysis_engine
         self.session_manager = session_manager
         self.database = database
+        self.raw_log_library = raw_log_library or RawLogLibrary()
 
     def build_breakin_controller(self):
         measurement_manager = MeasurementManager(
@@ -35,10 +37,11 @@ class ApplicationBuilder:
 
         analysis_engine = self.analysis_engine or AnalysisEngine()
 
-        return BreakinController(
+        return LocalRawLogBreakinController(
             serial_controller=self.serial_controller,
             measurement_manager=measurement_manager,
             analysis_engine=analysis_engine,
             database=self.database,
             session_manager=self.session_manager,
+            raw_log_library=self.raw_log_library,
         )
